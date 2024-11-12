dda46641e7aa9032bee1a49dbbe9432fbe297730
Nikita Matigorov <r.matigorov@yandex.ru>
2024-11-12 21:02:27 +0300

    style change groupname to groupcourses

--- src/Courses.Application.Contracts/Permissions/CoursesPermissions.cs
+++ src/Courses.Application.Contracts/Permissions/CoursesPermissions.cs
@@ -2,18 +2,19 @@ namespace Courses.Permissions;
 
 public static class CoursesPermissions
 {
-    public const string GroupName = "Courses";
+    public const string GroupCourses = "Courses";
 
 
-    
+
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
     public static class Courses
     {
-        public const string Default = GroupName + ".Courses";
+        public const string Default = GroupCourses + ".Courses";
         public const string Create = Default + ".Create";
         public const string Edit = Default + ".Edit";
         public const string Delete = Default + ".Delete";
     }
+
 
 }
